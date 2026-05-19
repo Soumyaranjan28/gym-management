d98cc07668f3cc71c472b23e4374ebc302cac68d
Soumyaranjan parida2 <ssoumya27681@gmail.com>
2026-05-19 15:38:49 +0530

    fixed auth api routes

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,6 +1,17 @@
+// import axios from "axios";
+
+// const API = "https://gym-backend-h2rw.onrender.com";
+
+// export const registerUser = (data) => {
+//   return axios.post(`${API}/register`, data);
+// };
+
+// export const loginUser = (data) => {
+//   return axios.post(`${API}/login`, data);
+// };
 import axios from "axios";
 
-const API = "https://gym-backend-h2rw.onrender.com";
+const API = "https://gym-backend-h2rw.onrender.com/api/auth";
 
 export const registerUser = (data) => {
   return axios.post(`${API}/register`, data);
